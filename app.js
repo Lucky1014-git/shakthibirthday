2c3d8af0aaa471f5ee69abae7364e84797384bc8
venkat venu <venkat_venugopalan@ezcorp.com>
2020-05-29 12:23:47 -0500

    u

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const port = 3000;
 
 app.get('/', function(req, res) {
-    res.send('Hello World!');
+	res.sendFile(path.join(__dirname + '/Shakthi.html'));
 });
 
-app.listen(process.env.PORT || port, () => console.log(`url-shortener listening on port ${port}!`));
+app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
